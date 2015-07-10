72db36f239cfcad40e1773824df9f24d5721f7ee
samuel <samuelbandeira@gmail.com>
2015-07-10 12:13:07 -0300

    Adicionando CSS - Utilizando a pasta vendor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Retorna um titulo base para as paginas do Pages Controller
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
