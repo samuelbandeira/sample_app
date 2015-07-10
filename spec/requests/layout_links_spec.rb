bb542363b73a684a6f94147df016b9074dc57491
samuel <samuelbandeira@gmail.com>
2015-07-10 17:05:36 -0300

    Testes dos links da tela

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -25,4 +25,18 @@ describe "LayoutLinks" do
     get signup_path
     response.should have_selector("title", :content => "Sign up")
   end
+  
+  it "should have a right link on the Root" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home" 
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+  end
 end
